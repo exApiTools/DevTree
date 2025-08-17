9b892be4d5d62d29fe8a3ffc77d1e01bdd417735
instant.sc <code@insc.cc>
2025-08-17 17:34:52 +0200

    Add stored custom expression display

--- DevTreeSettings.cs
+++ DevTreeSettings.cs
@@ -76,6 +76,9 @@ public class CustomExpressionSettings
 {
     public TextNode Expression { get; set; } = new TextNode("");
     public ToggleNode EvaluateEveryFrame { get; set; } = new ToggleNode(false);
+
+    [IgnoreMenu]
+    internal object EvaluatedObject { get; set; }
 }
 
 [Submenu(CollapsedByDefault = true, EnableSelfDrawCollapsing = true, RenderMethod = nameof(Render))]
